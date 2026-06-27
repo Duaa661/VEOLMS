3224a0543d77f89cd34e082e782e2c7fb3dc138d
Dushyant Rajput <dushyantr737@gmail.com>
2026-06-27 12:33:13 +0530

    Today i am completed login page ui

---
+++ marshal-lms/app/(auth)/login/page.tsx
@@ -0,0 +1,35 @@
+ import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { FaGithub } from "react-icons/fa";
+export default function LoginPage() {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle className="text-xl">Welcome Back</CardTitle>
+                <CardDescription>Login with your Github Email Account</CardDescription>
+            </CardHeader>
+            <CardContent className="flex flex-col gap-4">
+                <Button className="w-full" variant="outline">
+                    <FaGithub className="size-4"/>
+                  Sign in with Github
+                </Button>
+                {/* other content */}
+                <div className="relative text-center text-sm after:absolute
+                after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center
+                after:border-t after:border-border">
+            <span className="relative z-10 bg-card px-4 text-muted-foreground">
+              Or continue with</span>
+          </div>
+          <div className="grid gap-3">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input type="email" placeholder="Emter your Email"></Input>
+            </div>
+            <Button>Continue with Email</Button>
+          </div>
+            </CardContent>
+        </Card>
+    )
+}
