500133fbf59c43cde4625cd6d9673f38666d6e38
Dushyant Rajput <dushyantr737@gmail.com>
2026-07-11 12:58:17 +0530

    fix bug

--- marshal-lms/app/(public)/_components/Navbar.tsx
+++ marshal-lms/app/(public)/_components/Navbar.tsx
@@ -2,7 +2,6 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 import Logo from "@/public/lms.png";
 import { ThemeToggle } from "@/components/ui/themeToggle";
 import { authClient } from "@/lib/auth-client";
@@ -19,13 +18,18 @@ const Navbar = () => {
   const { data: session, isPending } = authClient.useSession();
 
   return (
-    <header
-      className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
-    >
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto flex min-h-16 items-center px-4 md:px-6 lg:px-8">
         <Link href="/" className="mr-6 flex items-center space-x-2">
-          <Image src={Logo} className="size-9" alt="logo" />
-          <span className="font-bold text-lg">VeoLMS.</span>
+          <Image
+            src={Logo}
+            alt="VeoLMS Logo"
+            width={36}
+            height={36}
+            className="size-9"
+            priority
+          />
+          <span className="text-lg font-bold">VeoLMS.</span>
         </Link>
 
         {/* Desktop Navigation */}
@@ -46,19 +50,7 @@ const Navbar = () => {
             <ThemeToggle />
 
             {isPending ? null : session ? (
-              <UserDropdown
-                email={session.user.email}
-                image={
-                  session?.user.image ??
-                      `https://avatar.vercel.sh/${session?.user.email}`
-                    
-                }
-                name={
-                  session?.user.name && session.user.name.length > 0
-                    ? session.user.name :
-                    session.user.email.split("@")[0]
-                }
-              />
+              <UserDropdown />
             ) : (
               <>
                 <Link
@@ -71,7 +63,7 @@ const Navbar = () => {
                 </Link>
 
                 <Link
-                  href="/login"
+                  href="/register"
                   className={buttonVariants()}
                 >
                   Get Started
