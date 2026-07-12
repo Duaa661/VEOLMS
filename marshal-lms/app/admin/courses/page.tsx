b7f08cfb879a76b5c4d02ba997f0c2de4f4a039d
Dushyant Rajput <dushyantr737@gmail.com>
2026-07-12 20:51:43 +0530

    fix typescript any error

--- marshal-lms/app/admin/courses/page.tsx
+++ marshal-lms/app/admin/courses/page.tsx
@@ -1,57 +1,75 @@
 import { admingetCourses } from "@/app/data/admin/admin-get-courses";
+import EmptyState from "@/components/general/EmptyState";
 import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 import React, { Suspense } from "react";
-import { AdminCourseCard, AdminCourseCardSkelton } from "./_components/AdminCourseCard";
-import EmptyState from "@/components/general/EmptyState";
+import {
+  AdminCourseCard,
+  AdminCourseCardSkelton,
+} from "./_components/AdminCourseCard";
 
-const page =  () => {
-  
+type CourseItem = {
+  id: string;
+  title: string;
+  description: string;
+  fileKey: string | null;
+  price: number;
+  level: string;
+  duration: number;
+  status: string;
+};
+
+const Page = () => {
   return (
     <>
       <div className="flex items-center justify-between">
         <h1 className="text-2xl font-bold">Your Courses</h1>
+
         <Link className={buttonVariants()} href="/admin/courses/create">
           Create Course
         </Link>
       </div>
+
       <div>
-        <Suspense fallback={<AdminCourseCardSkeltonLayout/>}>
+        <Suspense fallback={<AdminCourseCardSkeltonLayout />}>
           <RenderCourses />
-          </Suspense>
+        </Suspense>
       </div>
     </>
   );
 };
 
-export default page;
+export default Page;
 
 async function RenderCourses() {
-  const data = await admingetCourses()
+  const data: CourseItem[] = await admingetCourses();
+
   return (
-  <>
-        {
-          data.length === 0 ? <EmptyState
-            title="No Courses Found"
-            description="Create a new Course get started"
-            buttonText="Create Course"
-            href="/admin/courses/create"
-          /> : <div className="grid grid-cols-1 sm:grid-cl-2 md:grid-cols-1 lg:grid-cols-2 gap-7">
-          {data.map((course) => (
+    <>
+      {data.length === 0 ? (
+        <EmptyState
+          title="No Courses Found"
+          description="Create a new Course get started"
+          buttonText="Create Course"
+          href="/admin/courses/create"
+        />
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-7">
+          {data.map((course: CourseItem) => (
             <AdminCourseCard key={course.id} data={course} />
           ))}
-        </div>     
-      }
-      </>
-  )
+        </div>
+      )}
+    </>
+  );
 }
 
-function AdminCourseCardSkeltonLayout(){
+function AdminCourseCardSkeltonLayout() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cl-2 md:grid-cols-1 lg:grid-cols-2 gap-7">
-      {Array.from({ length: 4 }).map((_, index) => (
-        <AdminCourseCardSkelton key={index}/>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-7">
+      {Array.from({ length: 4 }).map((_, index: number) => (
+        <AdminCourseCardSkelton key={index} />
       ))}
-     </div>
-  )
+    </div>
+  );
 }
