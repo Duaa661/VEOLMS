da32b112173f4e7d14e08edcf386289c0281f975
Dushyant Chauhan <dushyantr737@gmail.com>
2026-07-16 23:37:58 +0530

    Update CoursesSlugRoute to load lesson content
    Refactor CoursesSlugRoute to fetch lesson content asynchronously and display it using CourseContent component.

--- marshal-lms/app/dashboard/[slug]/page.tsx
+++ marshal-lms/app/dashboard/[slug]/page.tsx
@@ -1,7 +1,24 @@
-export default function CoursesSlugRoute() {
-    return (
-        <div>
-            <h1>Hello world</h1>
-        </div>
-    )
+import { CourseContent } from "../_components/CourseContent";
+import { getLessonContent } from "@/app/data/course/get-lesson-content";
+
+interface Props {
+  params: Promise<{
+    slug: string;
+    lessonId: string;
+  }>;
+}
+
+export default async function CoursesSlugRoute({ params }: Props) {
+  const { slug, lessonId } = await params;
+
+  const data = await getLessonContent({
+    slug,
+    lessonId,
+  });
+
+  return (
+    <div className="flex h-full flex-col">
+      <CourseContent data={data} />
+    </div>
+  );
 }
