da4d36db78e4c8d61bc4f486ec01b46174110387
Dushyant Chauhan <dushyantr737@gmail.com>
2026-07-16 23:51:25 +0530

    Refactor CoursesSlugRoute to remove lesson content
    Removed lesson content fetching and updated component structure.

--- marshal-lms/app/dashboard/[slug]/page.tsx
+++ marshal-lms/app/dashboard/[slug]/page.tsx
@@ -1,21 +1,7 @@
-import { CourseContent } from "../_components/CourseContent";
-import { getLessonContent } from "@/app/data/course/get-lesson-content";
-
-interface Props {
-  params: Promise<{
-    slug: string;
-    lessonId: string;
-  }>;
-}
-
-export default async function CoursesSlugRoute({ params }: Props) {
-  const { lessonId } = await params;
-
-  const data = await getLessonContent(lessonId);
-
-  return (
-    <div className="flex h-full flex-col">
-      <CourseContent data={data} />
-    </div>
-  );
-}
+export default function CoursesSlugRoute() {
+return (
+<div>
+  
+</div>
+)
+} 
